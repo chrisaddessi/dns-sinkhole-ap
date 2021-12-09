eb3b2edae83a93b17c2a6d9f0ca2eaf6f2491943
chrisaddessi <73910801+chrisaddessi@users.noreply.github.com>
2021-12-09 02:41:25 +0000

    Delete dns-sinkhole-ap directory

--- dns-sinkhole-ap/install.sh
+++
@@ -1,7 +0,0 @@
-sudo apt install hostapd dnsmasq -y
-
-sudo cp -r usr-dns-sinkhole-ap* /usr/share/dns-sinkhole-ap
-sudo cp dns-sinkhole-ap.sh /usr/bin/dns-sinkhole-ap
-sudo cp dns-sinkhole-ap.service /etc/systemd/system/dns-sinkhole-ap.service
-sudo systemctl enable dns-sinkhole-ap
-echo "You will need to reboot your computer to start the dns-sinkhole-ap service."
--- dns-sinkhole-ap/usr-dns-sinkhole-ap/afterhostapd.sh
+++
@@ -1,5 +0,0 @@
-sudo pkill dnsmasq
-ifconfig replaceme up 192.168.1.254 netmask 255.255.255.0
-ifconfig replaceme mtu 1400
-sudo route add -net 192.168.1.0 netmask 255.255.255.0 gw 192.168.1.254
-dnsmasq -C /home/pi/Desktop/BlockerFiles/temp-run/dnsmasq.conf 
