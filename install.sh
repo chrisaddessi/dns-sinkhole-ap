3fd288ba81d26986c0230605c35fb2b8a4054485
chrisaddessi <73910801+chrisaddessi@users.noreply.github.com>
2021-12-10 05:25:55 +0000

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-sudo apt install dnsmasq hostapd iptables-persistent -y
-
-sudo cp -r usr-dns-sinkhole-ap* /usr/share/dns-sinkhole-ap
-sudo cp dns-sinkhole-ap.sh /usr/bin/dns-sinkhole-ap
-sudo mkdir /usr/share/dns-sinkhole-ap/temp-run
-sudo cp dns-sinkhole-ap.service /etc/systemd/system/dns-sinkhole-ap.service
-sudo systemctl enable dns-sinkhole-ap
-echo "You will need to reboot your computer to start the dns-sinkhole-ap service."
