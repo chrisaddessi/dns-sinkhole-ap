f8158d177213d70aeaf053443d26c9f69f9a51f0
chrisaddessi <73910801+chrisaddessi@users.noreply.github.com>
2021-12-10 05:27:23 +0000

    Delete usr-dns-sinkhole-ap directory

--- usr-dns-sinkhole-ap/afterhostapd.sh
+++
@@ -1,6 +0,0 @@
-sudo pkill dnsmasq
-ifconfig replaceme up 192.168.1.254 netmask 255.255.255.0
-ifconfig replaceme mtu 1400
-sudo route add -net 192.168.1.0 netmask 255.255.255.0 gw 192.168.1.254
-sudo chmod 666 /tmp/dnsmasq.log
-dnsmasq -C /usr/share/dns-sinkhole-ap/temp-run/dnsmasq.conf
--- usr-dns-sinkhole-ap/mon.sh
+++
@@ -1,4 +0,0 @@
-#sudo airmon-ng check kill
-sudo ip link set replaceme down
-sudo iw replaceme set monitor none
-sudo ip link set replaceme up
