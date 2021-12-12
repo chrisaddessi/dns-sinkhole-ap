d8f866a62a99aebd1f8026abf83ac0ee60430a42
chrisaddessi <73910801+chrisaddessi@users.noreply.github.com>
2021-12-12 18:17:24 +0000

    Delete usr-dns-sinkhole-ap directory

--- usr-dns-sinkhole-ap/ipforward.sh
+++
@@ -1,3 +0,0 @@
-iptables --table nat --append POSTROUTING --out-interface maininterface -j MASQUERADE
-iptables --append FORWARD --in-interface replaceme -j ACCEPT
-echo 1 > /proc/sys/net/ipv4/ip_forward
--- usr-dns-sinkhole-ap/preppinic.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev' > /etc/wpa_supplicant/wpa_supplicant.conf
-echo 'update_config=1' >> /etc/wpa_supplicant/wpa_supplicant.conf
-echo 'country=US' >> /etc/wpa_supplicant/wpa_supplicant.conf
